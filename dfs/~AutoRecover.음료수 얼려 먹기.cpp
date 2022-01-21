4fb93649e3b43ea1205f3fffaf4637895b53fd56
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-01-21 11:16:55 +0900

    dfs 문제 풀이

---
+++ dfs/~AutoRecover.음료수 얼려 먹기.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int n, m;
+int graph[1000][1000]; 
+
+bool dfs(int x, int y) {
+	if (x <= -1 || x >= n || y <= -1 || y >= m) {
+		return false;
+	}
+
+	if (graph[x][y] == 0) {
+		graph[x][y] = 1; //방문처리
+
+		dfs(x - 1, y); 
+		dfs(x, y - 1);
+		dfs(x, y + 1);
+		dfs(x + 1, y); 
+		return true; 
+	}
+	return false; 
+}
+
+int main(void) {
+
+	cin >> n >> m; 
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%d", &graph[i][j]); 
+		}
+	}
+
+	int result = 0; 
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (dfs(i, j)) {
+				result += 1; 
+			}
+		}
+	}
+	cout << result << '\n'; 
+}
