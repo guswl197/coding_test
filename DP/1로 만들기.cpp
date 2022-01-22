2c16779af4b2c925e1994e5e5fefe10ff47128e9
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-01-22 20:59:39 +0900

    코드 복습 및 수정

--- DP/1로 만들기.cpp
+++ DP/1로 만들기.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
-int d[30001];
+int d[30001]; 
 int x;
 
 int main(void) {
- 
 	cin >> x; 
-
-	d[0] = 0;
-	d[1] = 0;
+	d[0] = 0; 
+	d[1] = 0; 
 
 	for (int i = 2; i <= x; i++) {
 		d[i] = d[i - 1] + 1;
 
 		if (i % 5 == 0) {
-			d[i] = min(d[i], d[i / 5] + 1);
+			d[i] = min(d[i / 5] + 1, d[i]); 		
 		}
 		if (i % 3 == 0) {
-			d[i] = min(d[i], d[i / 3] + 1);
+			d[i] = min(d[i / 3] + 1, d[i]);
 		}
 		if (i % 2 == 0) {
-			d[i] = min(d[i], d[i / 2] + 1);
+			d[i] = min(d[i / 2] + 1, d[i]);
 		}
 	}
 
-	printf("%d\n", d[x]); 
-	return 0; 
-
+	cout << d[x] << '\n'; 
 }
