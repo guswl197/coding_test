c94a2d6e2468f7e27c7b122600d4d78a33878208
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-01-22 21:00:53 +0900

    코드 복습 및 수정

--- DP/효율적인 화폐 구성.cpp
+++ DP/효율적인 화폐 구성.cpp
@@ -1,41 +1,37 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <string.h>
+#include <algorithm>
 
-using namespace std; 
+using namespace std;
 
-int n, m; 
-vector<int> arr;
+int n, m;
+vector<int> v;
 
 int main(void) {
-
 	cin >> n >> m; 
+	vector<int> d(m + 1, 1001);
+
 	for (int i = 0; i < n; i++) {
 		int x; 
 		cin >> x;
-		arr.push_back(x); 
+		v.push_back(x); 
 	}
 
-	vector<int> d(m + 1, 1001); 
-
-	d[0] = 0; 
+	d[0] = 0;
 	
-	for (int i = 0; i < n; i++) {
-		for (int j = arr[i]; j <= m; j++) {
-
-			if (d[j - arr[i]] != 1001) {
-				d[j] = min(d[j], d[j - arr[i]] + 1); 
+	for (int i = 1; i <= m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i - v[j] >= 0) {
+				d[i] = min(d[i - v[j]] + 1, d[i]); 
 			}
 		}
 	}
-		
-	if (d[m] >=1001) {
+	
+	if (d[m] >= 1001) {
 		cout << -1 << '\n';
 	}
 	else {
 		cout << d[m] << '\n';
 	}
-	return 0; 
-
+	return 0;
 }
